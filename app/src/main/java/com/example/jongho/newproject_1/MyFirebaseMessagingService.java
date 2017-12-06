ad36f81ee8ee90785dcca4457e4690a95297ea75
김종현 <whdgus963@naver.com>
2017-12-06 17:52:29 +0900

    add notification icon, FCM can use API26 version

--- app/src/main/java/com/example/jongho/newproject_1/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/jongho/newproject_1/MyFirebaseMessagingService.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -19,26 +20,31 @@ import java.util.Map;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
         if(remoteMessage.getData().size() > 0) {
+            Log.d("haha", "Message data payload " + remoteMessage.getData());
+        }
+        if (remoteMessage.getNotification() != null){
+            Log.d("haha", "Message Notification Body" + remoteMessage.getNotification().getBody());
             Map<String, String> payload = remoteMessage.getData();
             showNotification(payload);
         }
     }
     private void showNotification(Map<String, String> payload){
-        NotificationCompat.Builder builder= new NotificationCompat.Builder(this);
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle(payload.get("username"));
-        builder.setContentText(payload.get("email"));
+        if(payload != null) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            builder.setContentTitle(payload.get("username"));
+            builder.setContentText(payload.get("email"));
 
-        Intent resultIntent = new Intent(this, MapsActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            Intent resultIntent = new Intent(this, MapsActivity.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
 
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(resultPendingIntent);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(resultPendingIntent);
 
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, builder.build());
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(0, builder.build());
+        }
     }
 }
