6d8b30c364d942710b7a77a07f41cdc8baf16ebb
김종현 <whdgus963@naver.com>
2017-12-07 00:05:32 +0900

    getItemActivity not finishe problem fixed

--- app/src/main/java/com/example/jongho/newproject_1/getItemActivity.java
+++ app/src/main/java/com/example/jongho/newproject_1/getItemActivity.java
@@ -3,9 +3,8 @@ package com.example.jongho.newproject_1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,17 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class getItemActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -89,8 +82,9 @@ public class getItemActivity extends AppCompatActivity
         editcontent.setText("");
         edittime.setText("");
 
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, MapsActivity.class);
+//        startActivity(intent);
+        finish();
     }
 
 
