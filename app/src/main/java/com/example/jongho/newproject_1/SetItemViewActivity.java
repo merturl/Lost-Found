74a3bc95d310212caca1057a5b36eaa19fec17b9
woong <ccw1021@naver.com>
2017-12-10 23:22:06 +0900

    update ItemImage

--- app/src/main/java/com/example/jongho/newproject_1/SetItemViewActivity.java
+++ app/src/main/java/com/example/jongho/newproject_1/SetItemViewActivity.java
@@ -2,7 +2,9 @@ package com.example.jongho.newproject_1;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,6 +25,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -30,6 +33,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class SetItemViewActivity extends AppCompatActivity {
     // Firebase 객체 생성
@@ -37,6 +41,7 @@ public class SetItemViewActivity extends AppCompatActivity {
     private StorageReference mStorageRef;
 
     private Bitmap bitmap;
+    private ImageView imageViewgetItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,17 @@ public class SetItemViewActivity extends AppCompatActivity {
             }
         });
 
+        //imgview에 리스너를 달아 이미지뷰 클릭시 이미지 추가를 함
+        imageViewgetItem = (ImageView) findViewById(R.id.imgv_setitem);
+        imageViewgetItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //이미지 선택할 수 있는 엑티비티 창 호출
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+
         Intent getintent = getIntent();
         final String DbRef = getintent.getStringExtra("DbRef");
         final String ImageRef = getintent.getStringExtra("ImageRef");
@@ -65,7 +81,6 @@ public class SetItemViewActivity extends AppCompatActivity {
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         mStorageRef = FirebaseStorage.getInstance().getReference();
                         StorageReference storageReference = mStorageRef.child(ImageRef+ ".jpg");
-//                mStorageRef.child("Item/image/"+mFirebaseAuth.getCurrentUser().getUid()+"/"+dataSnapshot.getKey() +".jpg");
 
                         Item item = dataSnapshot.getValue(Item.class);
 
@@ -98,23 +113,77 @@ public class SetItemViewActivity extends AppCompatActivity {
                 });
     }
 
-    public void EditItem(String Ref,String ImageRef, Item item) {
-        EditText title = (EditText) findViewById(R.id.edit_ItemTitle);
-        EditText content = (EditText) findViewById(R.id.edit_ItemContent);
-        EditText time = (EditText) findViewById(R.id.edit_ItemTime);
+    public void EditItem(final String Ref, final String ImageRef, final Item item) {
+
+        EditText etitle = (EditText) findViewById(R.id.edit_ItemTitle);
+        EditText econtent = (EditText) findViewById(R.id.edit_ItemContent);
+        EditText etime = (EditText) findViewById(R.id.edit_ItemTime);
         ImageView imgView = (ImageView)findViewById(R.id.imgv_setitem);
         Button BtnEdit = (Button)findViewById(R.id.btn_edit);
 
-        item.setTitle(title.getText().toString());
-        item.setContent(content.getText().toString());
-        item.setTime(time.getText().toString());
-        uploadImage(ImageRef);
+        final String title = etitle.getText().toString();
+        final String content = econtent.getText().toString();
+        final String time = etime.getText().toString();
+//        item.setTitle(title.getText().toString());
+//        item.setContent(content.getText().toString());
+//        item.setTime(time.getText().toString());
+        
+        // 저장 버튼 클릭시
+        BtnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SetItemViewActivity.this, "Hi btn", Toast.LENGTH_SHORT).show();
+                Log.d("DB","item.getContent=="+item.getContent());
+                Log.d("DB","Ref==="+Ref);
+                // 아이템 저장
+                DatabaseReference mFireRef = mFireDB.getReference(Ref);
+                try{
+                    mFireRef.setValue(new Item(item.getType(), item.getLat(), item.getLng(), title, content, time));
+                } catch (Exception e) {
+                    Log.d("DB","hihi db==="+Ref);
+                    e.printStackTrace();
+                }
+
+//                String postId = mFireRef.getKey();
+
+                // 이미지 저장
+                StorageReference reference = mStorageRef.child(ImageRef);
+
+                //파이어베이스에 쓰이는 데이터로 이미지 변환
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                try {
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                } catch ( NullPointerException e ) {
+                    return;
+                }
+                byte[] data = baos.toByteArray();
+
+                UploadTask uploadTask = reference.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        finish();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+//                Uri downloadUrl = taskSnapshot.getDownloadUrl(); //이미지가 저장된 주소의 URL
+                        Log.d("DB","success listiner"+item.getContent());
+                        uploadImage(ImageRef);
+                        Toast.makeText(SetItemViewActivity.this, "이미지 저장 성공", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 
     //파이어스토어에 이미지 저장
-    public boolean uploadImage(String ImageRef){
+    public void uploadImage(String ImageRef){
+        Log.d("DB","uploadImage start");
         if(ImageRef == null ) {
-            return false;
+            return;
         }
 
         //파이어스토어 접근 레퍼런스    // Item/image/uid/randomkey.jpg
@@ -126,7 +195,7 @@ public class SetItemViewActivity extends AppCompatActivity {
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         } catch ( NullPointerException e ) {
             Toast.makeText(this, "Don't image", Toast.LENGTH_SHORT).show();
-            return false;
+            return;
         }
         byte[] data = baos.toByteArray();
 
@@ -144,9 +213,36 @@ public class SetItemViewActivity extends AppCompatActivity {
                 // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
 //                Uri downloadUrl = taskSnapshot.getDownloadUrl(); //이미지가 저장된 주소의 URL
                 Toast.makeText(SetItemViewActivity.this, "이미지 저장 성공", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
-        return true;
     }
 
+    //이미지 선택할 수 있는 엑티비티 창 수행 결과
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode ==1 ){
+
+            if ( data == null) {
+                Toast.makeText(this, "Sorry, Don't find image", Toast.LENGTH_SHORT).show();
+            } else {
+                //img를 받기 위한 Uri
+                Uri image = data.getData();
+                Log.i(image.toString(), "select image////"+image.toString());
+                try {
+                    //사진을 비트맵이미지로 변환
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image);
+
+                    //이미지 뷰에 비트맵 부착
+                    imageViewgetItem.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
 }
