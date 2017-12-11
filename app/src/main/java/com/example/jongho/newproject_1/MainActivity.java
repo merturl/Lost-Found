f2a4dd52f58c7dab9db8ae0580c44d91b3e26a2b
김종현 <whdgus963@naver.com>
2017-12-11 14:20:14 +0900

    fixed confilt permissions

--- app/src/main/java/com/example/jongho/newproject_1/MainActivity.java
+++ app/src/main/java/com/example/jongho/newproject_1/MainActivity.java
@@ -579,18 +579,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         this.googleMap.animateCamera(CameraUpdateFactory.zoomOut());
         this.googleMap.setMinZoomPreference(17.0f);
         this.googleMap.setMaxZoomPreference(18.0f);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            startLocationPermissionRequest(REQUEST_PERMISSIONS_MYLOCATION_CODE);
-            return;
-        }
         // 마커 클릭 이벤트
         this.googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
@@ -640,7 +628,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         });
-
         this.googleMap.setOnMapClickListener(this);
     }
 
